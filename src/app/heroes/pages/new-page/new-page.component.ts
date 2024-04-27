a8bd093fea3156e3d4088aeebdbdad6a76d1e8b0
Juan <gurquiza8@gmail.com>
2024-04-27 14:59:55 -0500

    Show data for hero in edit page

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,14 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { FormControl, FormGroup } from '@angular/forms';
 import { Hero, Publisher } from '../../interfaces/hero.interface';
 import { HerosService } from '../../services/heroes.service';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-new-page',
   templateUrl: './new-page.component.html',
   styles: ``
 })
-export class NewPageComponent {
+export class NewPageComponent implements OnInit{
 
   public heroForm = new FormGroup({
     id: new FormControl<string>(''),
@@ -25,7 +27,25 @@ export class NewPageComponent {
     { id: 'Marvel Comics', desc: 'Marvel - Comics' }
   ]
 
-  constructor( private heroesService: HerosService ) {}
+  constructor(
+    private heroesService: HerosService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    if ( !this.router.url.includes('edit') ) return;
+
+    this.activatedRoute.params
+      .pipe(
+        switchMap( ({id}) => this.heroesService.getHeroById(id) ),
+      ).subscribe( hero => {
+        if ( !hero ) return this.router.navigateByUrl('/');
+
+        this.heroForm.reset( hero );
+        return;
+      })
+  }
 
   get currentHero(): Hero {
     const hero = this.heroForm.value as Hero;
@@ -34,6 +54,21 @@ export class NewPageComponent {
 
   onSubmit(): void {
     if ( this.heroForm.invalid ) return;
+
+    if ( this.currentHero.id ) {
+      this.heroesService.updateHero( this.currentHero )
+        .subscribe( hero => {
+          // TODO: Mostrar snackbar
+        });
+
+      return;
+    }
+
+    this.heroesService.addHero( this.currentHero )
+      .subscribe( hero => {
+        // TODO: Mostrar snackbas y navegar a /heroes/edit/ hero.id
+      });
+
   }
 
 }
